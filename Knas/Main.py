d2fba7a26dc50632706df7287169fbc0a0cfae1b
Sven SP <sven.starckparding@elev.ga.ntig.se>
2024-01-31 10:32:24 +0100

    knas

--- Knas/Main.py
+++ Knas/Main.py
@@ -1,7 +1,20 @@
 import csv
+from tkinter import Tk
 
 
 class loan_acoount():
     def __init__(self, username, password) -> None:
         self.username = username
         self.password = password
+         
+class InvalidPasswordError():
+    def __init__(self,password):
+        self.password = password
+        super().__init__(self.password)
+        
+class InvalidUsernameError():
+    def __init__ (self,username):
+        self.username = username 
+        super().__init__(self.username) 
+        
+
